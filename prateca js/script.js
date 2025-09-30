4bc12f491ac838b5968452beef60b8802afe6f3e
tavres026 <tmariaclara026@gmail.com>
2025-09-30 15:28:02 -0300

    Add files via upload

---
+++ prateca js/script.js
@@ -0,0 +1,14 @@
+//arrays -> vetores, matrizes
+// estrutura de dados para armazenar um conjunto de elementos
+
+let frutas = ["bananas", "maçã", "melancia",]; //array tamanho 3
+console.log(frutas);
+console.log(frutas[1]);
+
+let numeros = new Array(10,2,6,7);
+console.log(numeros);
+
+let arrayVazio = [];
+console.log(arrayVazio);
+arrayVazio[0] = "gaby";
+console.log(arrayVazio);
